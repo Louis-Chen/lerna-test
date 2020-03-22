03a28070f48557e1213bff1a6ba1db7be4382595
LuisChen <leo5916267@gmail.com>
2020-03-22 15:01:46 +0800

    add test component

---
+++ packages/head/__tests__/head.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const head = require('..');
+
+describe('head', () => {
+    it('needs tests');
+});
---
+++ packages/head/lib/head.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = head;
+
+function head() {
+    // TODO
+}
